dc9721e877b8135cf2852f2b9aa7ef1a78a0cfc1
sdk-team <sdk-team@alibabacloud.com>
2020-11-12 02:38:59 +0000

    Support OpenAPI.

---
+++ aliyun-java-sdk-openanalytics-open-v5/src/main/java/com/aliyuncs/v5/openanalytics_open/transform/v20180619/ListSparkJobResponseUnmarshaller.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.v5.openanalytics_open.transform.v20180619;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.v5.openanalytics_open.model.v20180619.ListSparkJobResponse;
+import com.aliyuncs.v5.openanalytics_open.model.v20180619.ListSparkJobResponse.DataResult;
+import com.aliyuncs.v5.openanalytics_open.model.v20180619.ListSparkJobResponse.DataResult.Data;
+import com.aliyuncs.v5.transform.UnmarshallerContext;
+
+
+public class ListSparkJobResponseUnmarshaller {
+
+	public static ListSparkJobResponse unmarshall(ListSparkJobResponse listSparkJobResponse, UnmarshallerContext _ctx) {
+		
+		listSparkJobResponse.setRequestId(_ctx.stringValue("ListSparkJobResponse.RequestId"));
+
+		DataResult dataResult = new DataResult();
+		dataResult.setPageNumber(_ctx.stringValue("ListSparkJobResponse.DataResult.PageNumber"));
+		dataResult.setPageSize(_ctx.stringValue("ListSparkJobResponse.DataResult.PageSize"));
+		dataResult.setTotalCount(_ctx.stringValue("ListSparkJobResponse.DataResult.TotalCount"));
+
+		List<Data> jobListList = new ArrayList<Data>();
+		for (int i = 0; i < _ctx.lengthValue("ListSparkJobResponse.DataResult.JobList.Length"); i++) {
+			Data data = new Data();
+			data.setCreateTime(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].CreateTime"));
+			data.setCreateTimeValue(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].CreateTimeValue"));
+			data.setDetail(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].Detail"));
+			data.setDriverResourceSpec(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].DriverResourceSpec"));
+			data.setExecutorInstances(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].ExecutorInstances"));
+			data.setExecutorResourceSpec(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].ExecutorResourceSpec"));
+			data.setJobId(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].JobId"));
+			data.setJobName(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].JobName"));
+			data.setSparkUI(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].SparkUI"));
+			data.setStatus(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].Status"));
+			data.setSubmitTime(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].SubmitTime"));
+			data.setSubmitTimeValue(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].SubmitTimeValue"));
+			data.setUpdateTime(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].UpdateTime"));
+			data.setUpdateTimeValue(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].UpdateTimeValue"));
+			data.setVcName(_ctx.stringValue("ListSparkJobResponse.DataResult.JobList["+ i +"].VcName"));
+
+			jobListList.add(data);
+		}
+		dataResult.setJobList(jobListList);
+		listSparkJobResponse.setDataResult(dataResult);
+	 
+	 	return listSparkJobResponse;
+	}
+}
---
+++ aliyun-java-sdk-openanalytics-open-v5/src/main/java/com/aliyuncs/v5/openanalytics_open/transform/v20180619/QueryDataSourceDefResponseUnmarshaller.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.v5.openanalytics_open.transform.v20180619;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.v5.openanalytics_open.model.v20180619.QueryDataSourceDefResponse;
+import com.aliyuncs.v5.openanalytics_open.model.v20180619.QueryDataSourceDefResponse.RegionListItem;
+import com.aliyuncs.v5.transform.UnmarshallerContext;
+
+
+public class QueryDataSourceDefResponseUnmarshaller {
+
+	public static QueryDataSourceDefResponse unmarshall(QueryDataSourceDefResponse queryDataSourceDefResponse, UnmarshallerContext _ctx) {
+		
+		queryDataSourceDefResponse.setRequestId(_ctx.stringValue("QueryDataSourceDefResponse.RequestId"));
+		queryDataSourceDefResponse.setRegionId(_ctx.stringValue("QueryDataSourceDefResponse.RegionId"));
+
+		List<RegionListItem> regionListList = new ArrayList<RegionListItem>();
+		for (int i = 0; i < _ctx.lengthValue("QueryDataSourceDefResponse.RegionList.Length"); i++) {
+			RegionListItem regionListItem = new RegionListItem();
+			regionListItem.setDataSourceName(_ctx.stringValue("QueryDataSourceDefResponse.RegionList["+ i +"].dataSourceName"));
+			regionListItem.setRoleArn(_ctx.stringValue("QueryDataSourceDefResponse.RegionList["+ i +"].roleArn"));
+			regionListItem.setAuthorizedURL(_ctx.stringValue("QueryDataSourceDefResponse.RegionList["+ i +"].authorizedURL"));
+
+			regionListList.add(regionListItem);
+		}
+		queryDataSourceDefResponse.setRegionList(regionListList);
+	 
+	 	return queryDataSourceDefResponse;
+	}
+}
