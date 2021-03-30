0ee5aba1b416b063b816bc8d8f0f2e8f13914620
sdk-team <sdk-team@alibabacloud.com>
2021-03-30 06:53:29 +0000

    Supported Open Api.

---
+++ aliyun-java-sdk-openanalytics-open-v5/src/main/java/com/aliyuncs/v5/openanalytics_open/model/v20180619/TagResourcesRequest.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.v5.openanalytics_open.model.v20180619;
+
+import com.aliyuncs.v5.RpcAcsRequest;
+import java.util.List;
+import com.aliyuncs.v5.http.MethodType;
+import com.aliyuncs.v5.openanalytics_open.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class TagResourcesRequest extends RpcAcsRequest<TagResourcesResponse> {
+	   
+
+	private List<String> resourceId;
+
+	private String resourceType;
+
+	private List<Tag> tag;
+	public TagResourcesRequest() {
+		super("openanalytics-open", "2018-06-19", "TagResources", "openanalytics");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.v5.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.v5.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public List<String> getResourceId() {
+		return this.resourceId;
+	}
+
+	public void setResourceId(List<String> resourceId) {
+		this.resourceId = resourceId;	
+		if (resourceId != null) {
+			for (int i = 0; i < resourceId.size(); i++) {
+				putQueryParameter("ResourceId." + (i + 1) , resourceId.get(i));
+			}
+		}	
+	}
+
+	public String getResourceType() {
+		return this.resourceType;
+	}
+
+	public void setResourceType(String resourceType) {
+		this.resourceType = resourceType;
+		if(resourceType != null){
+			putQueryParameter("ResourceType", resourceType);
+		}
+	}
+
+	public List<Tag> getTag() {
+		return this.tag;
+	}
+
+	public void setTag(List<Tag> tag) {
+		this.tag = tag;	
+		if (tag != null) {
+			for (int depth1 = 0; depth1 < tag.size(); depth1++) {
+				putQueryParameter("Tag." + (depth1 + 1) + ".Key" , tag.get(depth1).getKey());
+				putQueryParameter("Tag." + (depth1 + 1) + ".Value" , tag.get(depth1).getValue());
+			}
+		}	
+	}
+
+	public static class Tag {
+
+		private String key;
+
+		private String value;
+
+		public String getKey() {
+			return this.key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public String getValue() {
+			return this.value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
+
+	@Override
+	public Class<TagResourcesResponse> getResponseClass() {
+		return TagResourcesResponse.class;
+	}
+
+}
